980e625609265ebf73296759085b60799a8565ad
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2022-08-24 23:23:26 +0530

    event

---
+++ src/event.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class Event extends Component{
+    constructor(props){
+        super();
+        this.state = 0;
+    }
+
+    render(){
+        return(
+            <>
+            <h1>Hello world</h1>
+            </>
+        )
+    }
+}
