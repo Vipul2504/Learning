9d956fad6069af6b048d13d9e1b42f5e63e3cf94
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-07 00:54:17 +0530

    done

--- src/Components/Expense.jsx
+++ src/Components/Expense.jsx
@@ -1,16 +1,16 @@
 import './Expense.css'
+import ExpenseDate from './ExpenseDate';
 
-function Expense(props){
-    
+function ExpenseItem(props) {
     return (
-        <div className="expense-item">
-            <div>{ExpenseDate.toUTCString()}</div>
-            <div className="expense-item__description">
-                <h2>{ExpenseTitle}</h2>
-                <div className="expense-item__price">${ExpenseAmnt}</div>
-            </div>
+      <div className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
         </div>
-    )
-}
-
-export default Expense
+      </div>
+    );
+  }
+  
+  export default ExpenseItem;
